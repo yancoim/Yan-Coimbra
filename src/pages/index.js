1495e082b65396ab146bbb03b0606a88fc7bea53
yancoim <yancoim@gmail.com>
2023-03-23 23:51:28 -0300

    add icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,7 @@ export default function Home() {
       <Head>
         <title>Yan Coimbra</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/svg+xml" href="/coding.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
